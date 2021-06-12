d97b15f49a9ebe8ba0288c0e167ff9403c8f6a17
Benjamin Riemer <52243092+benriemer@users.noreply.github.com>
2021-06-12 05:20:30 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAzokLRs7_gb-7rxL21hAMOtr5It7yFz8Q',
-    authDomain: 'tik-tok-clone-85389.firebaseapp.com',
-    projectId: 'tik-tok-clone-85389',
-    storageBucket: 'tik-tok-clone-85389.appspot.com',
-    messagingSenderId: '531526019475',
-    appId: '1:531526019475:web:cddef1190bdf05beaa6066',
-    measurementId: 'G-0HR4TNPKEW'
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
